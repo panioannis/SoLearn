7b8a5a3ba236f85d27a1d0d9157a883122268aeb
panioannis <panioannis01@gmail.com>
2024-02-09 19:22:27 +0000

    Working run script with annotations and comments

--- run.sh
+++ run.sh
@@ -3,75 +3,59 @@
 # Define the number of clients
 
 set +m
-
 N=$1
-
 R=$2
-
 cd ./onchain/js/ || exit
+# =======================================================================================
+# ------------------------------ Start running stuff ------------------------------------ 
+# =======================================================================================
 
-# Start the server.ts with N clients
+# ------------------------------ Start the server.ts with N clients ---------------------
 ts-node server.ts $N | tee Global_model.out &
-
+# ------------------------------ Store the server pid -----------------------------------
 server_pid=$!
-
-# Wait for 5 seconds
+# ------------------------------ Wait for 3 seconds (to lower) --------------------------
 sleep 3
-
+# ------------------------------ Store the client pids ----------------------------------
 client_pids=()
-
-# Execute main.ts for each client
+# ------------------------------ Execute main.ts for each client ------------------------
 for ((i = 0; i < N; i++)); do
     ts-node main.ts $i | tee Client_model"$i".out &
     client_pids[$i]=$!
 done
-
 cd ../../
-
-echo  "${client_pids[@]}"
-
-# Wait for 2 seconds
+#echo  "${client_pids[@]}"
+# ------------------------------ Wait for 2 seconds -------------------------------------
 sleep 2
-
-# Start server.py
+# ------------------------------ Start server.py ----------------------------------------
 cd ./fl_server/ || exit
-
-# Execute client.py for each client
+# ------------------------------ Execute client.py for each client ----------------------
 for ((i = 0; i < N; i++)); do
     python client.py --node-id $i &
 done
-
 python server.py --rounds $R  
-
+# ------------------------------ Wait for 2 seconds (to lower) --------------------------
 sleep 2
-
-echo " Geia sou ! "
-
-# Define an array
-# my_array=(10 20 30 40 50)
-
+# ------------------------------ Reduce pid by one for everyone -------------------------
+# ------------------------------ Reduce pid by one for server   -------------------------
 server_pid=$((server_pid - 1))
-
-# Iterate over the elements of the array
+# ------------------------------ Reduce pid by one for clients  -------------------------
 for (( i = 0; i < ${#client_pids[@]}; i++ )); do
     # Subtract one from each element and store the result back in the array
     client_pids[i]=$(( ${client_pids[i]} - 1 ))
 done
-
 echo " $server_pid ${client_pids[@]} " 
-
-# trap "kill $server_pid ${client_pids[@]}; exit 1" INT
-# wait
-
+# =======================================================================================
+# ------------------------------ Start killing stuff ------------------------------------ 
+# =======================================================================================
+# ------------------------------ kill the server with SIGINT ----------------------------
 kill -SIGINT $server_pid
-
-echo Waiting for server to finish
+# ------------------------------ wait for server to die ---------------------------------
 wait $server_pid
-
+# ------------------------------ kill the clients with SIGINT ---------------------------
 kill -SIGINT "${client_pids[@]}" 
-
+# ------------------------------ wait for clients to die --------------------------------
 for (( i = 0; i < ${#client_pids[@]}; i++ )); do
-    # Subtract one from each element and store the result back in the array
     wait ${client_pids[i]}
 done
 
