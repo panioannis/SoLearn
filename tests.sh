12e7f3bbbe688385a646c41b04a80c7f706d21f9
panioannis <155060223+panioannis@users.noreply.github.com>
2024-02-13 19:07:08 +0000

    Create test script for running tests for clients and servers

---
+++ tests.sh
@@ -0,0 +1,23 @@
+# =======================================================================================
+# ------------------------------ Start running tests ------------------------------------ 
+# =======================================================================================
+
+#!/bin/bash
+mkdir -p ./experiments
+# Outer loop for clients from 2 to 10
+for clients in {10..100..10}; do
+    # Inner loop for rounds from 100 to 1000
+    # for rounds in {10..50..10}; do
+    for rounds in {100..1000..100}; do
+        # Start time
+        start=$(date +%s.%N)
+        # Run the command with current clients and rounds
+        ./run.sh $clients $rounds
+        # End time
+        end=$(date +%s.%N)
+        # Calculate elapsed time
+        runtime=$(echo "$end - $start" | bc)
+        # Print elapsed time
+        echo "Total runtime: $runtime seconds" >> ./experiments/Experiment_${clients}_clients_${rounds}_rounds/time.out
+    done
+done
