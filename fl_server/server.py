dab4af14770a46e263ed2315d5734e049b0aa308
panioannis <155060223+panioannis@users.noreply.github.com>
2024-02-13 14:33:37 +0000

    Update server.py to accomodate hyperparameter clients

--- fl_server/server.py
+++ fl_server/server.py
@@ -1,19 +1,35 @@
+import argparse
+import warnings
 from collections import OrderedDict
-from typing import Callable, Dict, List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple
 
 import flwr as fl
+from flwr_datasets import FederatedDataset
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, Normalize, ToTensor
+from tqdm import tqdm
+
+from io import BytesIO
+import random
+import numpy as np
+import requests
+import json
+import pickle
 
 from flwr.common import (
+    Code,
     EvaluateIns,
     EvaluateRes,
     FitIns,
     FitRes,
-    MetricsAggregationFn,
-    NDArrays,
-    Parameters,
-    Scalar,
+    GetParametersIns,
+    GetParametersRes,
+    Status,
     ndarrays_to_parameters,
-    parameters_to_ndarrays,
+    parameters_to_ndarrays
 )
 
 from flwr.proto.transport_pb2 import (
@@ -31,38 +47,22 @@ from flwr.common.serde import (
     parameters_from_proto
 )
 
-import numpy as np
-from io import BytesIO
-import random
-import numpy as np
-import requests
-import json
-import pickle
-
-import argparse
-
-import flwr as fl
-from flwr_datasets import FederatedDataset
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
-print(
-    f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
-)
-
-from flwr.server.client_manager import ClientManager
-from flwr.server.client_proxy import ClientProxy
-from flwr.server.strategy.aggregate import aggregate, weighted_loss_avg
-
 # Server configuration
-# host = '127.0.0.1'
-# port = 23456
-DEVICE = torch.device("cpu")
-class Net(nn.Module):
-    """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
+host = '127.0.0.1'
+port = 12345
 
+# https://github.com/adap/flower/blob/main/doc/source/tutorial-series-use-a-federated-learning-strategy-pytorch.ipynb
+
+# #############################################################################
+# 1. Regular PyTorch pipeline: nn.Module, train, test, and DataLoader
+# #############################################################################
+
+warnings.filterwarnings("ignore", category=UserWarning)
+#DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cpu")
+
+class Net(nn.Module):
+ #  LeNet 5 implementation
     def __init__(self) -> None:
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
@@ -81,46 +81,78 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-def get_parameters(net) -> List[np.ndarray]:
+def get_model_parameters(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
-def set_parameters(net, parameters: List[np.ndarray]):
+def set_model_parameters(net, parameters: List[np.ndarray]):
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
 
+def train(net, trainloader, epochs):
+    """Train the model on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    for _ in range(epochs):
+        for batch in tqdm(trainloader, "Training"):
+            images = batch["img"]
+            labels = batch["label"]
+            optimizer.zero_grad()
+            criterion(net(images.to(DEVICE)), labels.to(DEVICE)).backward()
+            optimizer.step()
 
-from typing import Callable, Union
+def test(net, testloader):
+    """Validate the model on the test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, loss = 0, 0.0
+    with torch.no_grad():
+        for batch in tqdm(testloader, "Testing"):
+            images = batch["img"].to(DEVICE)
+            labels = batch["label"].to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    accuracy = correct / len(testloader.dataset)
+    return loss, accuracy
 
-from flwr.common import (
-    EvaluateIns,
-    EvaluateRes,
-    FitIns,
-    FitRes,
-    MetricsAggregationFn,
-    NDArrays,
-    Parameters,
-    Scalar,
-    ndarrays_to_parameters,
-    parameters_to_ndarrays,
-)
-from flwr.server.client_manager import ClientManager
-from flwr.server.client_proxy import ClientProxy
-from flwr.server.strategy.aggregate import aggregate, weighted_loss_avg
+def load_data(node_id):
+    """Load partition CIFAR10 data."""
+    fds = FederatedDataset(dataset="cifar10", partitioners={"train": 10})
+    partition = fds.load_partition(node_id)
+    # Divide data on each node: 80% train, 20% test
+    partition_train_test = partition.train_test_split(test_size=0.2)
+    pytorch_transforms = Compose(
+        [ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    def apply_transforms(batch):
+        """Apply transforms to the partition from FederatedDataset."""
+        batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
+        return batch
+
+    partition_train_test = partition_train_test.with_transform(apply_transforms)
+    trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
+    testloader = DataLoader(partition_train_test["test"], batch_size=32)
+    return trainloader, testloader
+
+# #############################################################################
+# 2. Federation of the pipeline with Flower
+# #############################################################################
 
 # Get node id
 parser = argparse.ArgumentParser(description="Flower")
 parser.add_argument(
-    "--rounds",
+    "--node-id",
+    choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     required=True,
     type=int,
-    help="Number of rounds participating nodes in the System.",
+    help="Partition of the dataset divided into 10 iid partitions created artificially.",
 )
 
-rounds = parser.parse_args().rounds                                                      
+node_id = parser.parse_args().node_id
 
-def send_model(weights):
-    url = f"http://127.0.0.1:5000/api/post"
+def send_weights(weights):
+    url = f"http://127.0.0.1:300{node_id}/api/post"
     serialized_model = pickle.dumps(weights)
     response = requests.post(url, data=serialized_model, headers={'Content-Type': 'application/octet-stream'})
 
@@ -128,17 +160,14 @@ def send_model(weights):
     if response.status_code == 200:
         # Extracting the JSON body from the response
         #print("Ok \n")
-        loaded_data = pickle.loads(response.content)
-        #print(loaded_data)
+        return
+        #loaded_data = pickle.loads(response.content)
     else:
         print("POST request failed with status code:", response.status_code)
+    return 
 
-    #loaded_data = pickle.loads(response.content)
-    return loaded_data
-
-
-def receive_weights(node_id):
-    url = f"http://127.0.0.1:300{node_id}/api/post_get_latest_model"
+def receive_weights():
+    url = f"http://127.0.0.1:13256/api/post_get_latest_model"
 
     response = requests.post(url)
 
@@ -152,6 +181,8 @@ def receive_weights(node_id):
     print(received_data)
 
     node_url = "https://gateway.irys.xyz/" + received_data
+
+    # received_data = f"https://gateway.irys.xyz/C4PJqg6fB0jRU6THcpx7VtIbWpbRpIe0RoqMrqUE738"
 
     response = requests.get(node_url)
 
@@ -179,200 +210,130 @@ def receive_weights(node_id):
     else:
         print("POST request failed with status code:", response.status_code)
 
-    #print(loaded_model)    
+    #print(loaded_model)
+    return loaded_model 
 
-    return loaded_model  
+class FlowerClient(fl.client.Client):
+    def __init__(self, cid, net, trainloader, valloader):
+        self.cid = cid
+        self.net = net
+        self.trainloader = trainloader
+        self.valloader = valloader
 
+    def get_parameters(self, ins: GetParametersIns) -> GetParametersRes:
+        print(f"[Client {self.cid}] get_parameters")
 
-class FedCustom(fl.server.strategy.Strategy):
-    def __init__(
-        self,
-        fraction_fit: float = 1.0,
-        fraction_evaluate: float = 1.0,
-        min_fit_clients: int = 2,
-        min_evaluate_clients: int = 2,
-        min_available_clients: int = 2,
-    ) -> None:
-        super().__init__()
-        self.fraction_fit = fraction_fit
-        self.fraction_evaluate = fraction_evaluate
-        self.min_fit_clients = min_fit_clients
-        self.min_evaluate_clients = min_evaluate_clients
-        self.min_available_clients = min_available_clients
+        # Get parameters as a list of NumPy ndarray's
+        print("I am running from inside get parameters\n")
+        ndarrays: List[np.ndarray] = get_model_parameters(self.net)
 
-    def __repr__(self) -> str:
-        return "FedCustom"
+        # Serialize ndarray's into a Parameters object
+        parameters = ndarrays_to_parameters(ndarrays)
 
-    def initialize_parameters(
-        self, client_manager: ClientManager
-    ) -> Optional[Parameters]:
-        """Initialize global model parameters."""
-        net = Net()
-        ndarrays = get_parameters(net)
-        weights = ndarrays_to_parameters(ndarrays)
-        send_model(weights)
-        return weights 
-
-    def configure_fit(
-        self, server_round: int, parameters: Parameters, client_manager: ClientManager
-    ) -> List[Tuple[ClientProxy, FitIns]]:
-        """Configure the next round of training."""
-
-        # Sample clients
-        sample_size, min_num_clients = self.num_fit_clients(
-            client_manager.num_available()
-        )
-        clients = client_manager.sample(
-            num_clients=sample_size, min_num_clients=min_num_clients
+        status = Status(code=Code.OK, message="Success")
+        return GetParametersRes(
+            status=status,
+            parameters=parameters,
         )
 
-        # Create custom configs
-        n_clients = len(clients)
-        half_clients = n_clients // 2
-        standard_config = {"lr": 0.001}
-        higher_lr_config = {"lr": 0.003}
-        fit_configurations = []
-        for idx, client in enumerate(clients):
-            if idx < half_clients:
-                fit_configurations.append((client, FitIns(parameters, standard_config)))
-            else:
-                fit_configurations.append(
-                    (client, FitIns(parameters, higher_lr_config))
-                )
-        return fit_configurations
+    def fit(self, ins: FitIns) -> FitRes:
+        print(f"[Client {self.cid}] fit, config: {ins.config}")
 
-    def aggregate_fit(
-        self,
-        server_round: int,
-        results: List[Tuple[ClientProxy, FitRes]],
-        failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
-    ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
-        """Aggregate fit results using weighted average."""
+        # Deserialize parameters to NumPy ndarray's
+        #parameters_original = ins.parameters
+        #ndarrays_original = parameters_to_ndarrays(parameters_original)
 
-        #-------------------------------------------------------------------
-        # Start ADDED
+        received_weights = receive_weights()
+        parameters_original = pickle.loads(received_weights)
+        ndarrays_original = parameters_to_ndarrays(parameters_original)
 
-        # ------------------------ Sample Code -----------------------------
-
-        weights_loaded = []
-
-        for i in range(0,len(results)):
-            weights_loaded.append(pickle.loads(receive_weights(i)))
-
-        # End ADDED
-        #-------------------------------------------------------------------
-
-        weights_results = [
-            (parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples)
-            for _, fit_res in results
-        ]
+        # Update local model, train, get updated parameters
+        set_model_parameters(self.net, ndarrays_original)
+        train(self.net, self.trainloader, epochs=1)
         
-        #-------------------------------------------------------------------
-        # Start ADDED
-
-        updated_weights_results = []
-
-        for loaded_params, (_, fit_res) in zip(weights_loaded, results):
-            updated_weights_results.append((parameters_to_ndarrays(loaded_params), fit_res.num_examples))
-
-        # Now 'updated_weights_results' contains the updated first elements
-        weights_results = updated_weights_results
-
-        # End ADDED
-        #-------------------------------------------------------------------
-
-        loaded_weights = aggregate(updated_weights_results)
-
-        parameters_aggregated = ndarrays_to_parameters(loaded_weights)
+        # Get model parameters of the updated model.
+        ndarrays_updated = get_model_parameters(self.net)
+        # Serialize ndarray's into a Parameters object
+        parameters_updated = ndarrays_to_parameters(ndarrays_updated)
 
         #-------------------------------------------------------------------
         # Start ADDED
 
-        send_model(parameters_aggregated)
+        #name = f"model{self.cid}.pt"
+        #torch.save(self.net.state_dict(), name)
+        #torch.save(self.net.state_dict(), bytes_buffer)
+
+        bytes_buffer = BytesIO()
+        pickle.dump(parameters_updated, bytes_buffer)
+
+        bytes_buffer.seek(0)
+
+        url = f"http://127.0.0.1:300{self.cid}/api/post"
+        response = requests.post(url, data=bytes_buffer, headers={'Content-Type': 'application/octet-stream'})
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Extracting the JSON body from the response
+            # received_bytes_buffer = BytesIO(response.content)
+            # print(bytes_buffer.getbuffer().nbytes)
+            status = Status(code=Code.OK, message="Success")
+        else:
+            print("POST request failed with status code:", response.status_code)
+            status = Status(code=Code.OK, message="Success")
+        # bytes_buffer.seek(0)
+    
+        # received_bytes_buffer.seek(0)
+        
+        # parameters_updated = pickle.load(received_bytes_buffer)
+        
+        # #self.net.load_state_dict(model_state_dict)
+
+        # bytes_buffer.close()
+        # received_bytes_buffer.close()
+
 
         # End ADDED
         #-------------------------------------------------------------------
 
-
-
-        metrics_aggregated = {}
-        return parameters_aggregated, metrics_aggregated
-
-    def configure_evaluate(
-        self, server_round: int, parameters: Parameters, client_manager: ClientManager
-    ) -> List[Tuple[ClientProxy, EvaluateIns]]:
-        """Configure the next round of evaluation."""
-        if self.fraction_evaluate == 0.0:
-            return []
-        config = {}
-        evaluate_ins = EvaluateIns(parameters, config)
-
-        # Sample clients
-        sample_size, min_num_clients = self.num_evaluation_clients(
-            client_manager.num_available()
-        )
-        clients = client_manager.sample(
-            num_clients=sample_size, min_num_clients=min_num_clients
+        # Build and return response
+        #status = Status(code=Code.OK, message="Success")
+        return FitRes(
+            status=status,
+            parameters=parameters_updated,
+            num_examples=len(self.trainloader),
+            metrics={},
         )
 
-        # Return client/config pairs
-        return [(client, evaluate_ins) for client in clients]
+    def evaluate(self, ins: EvaluateIns) -> EvaluateRes:
+        print(f"[Client {self.cid}] evaluate, config: {ins.config}")
 
-    def aggregate_evaluate(
-        self,
-        server_round: int,
-        results: List[Tuple[ClientProxy, EvaluateRes]],
-        failures: List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]],
-    ) -> Tuple[Optional[float], Dict[str, Scalar]]:
-        """Aggregate evaluation losses using weighted average."""
+        # Deserialize parameters to NumPy ndarray's
+        parameters_original = ins.parameters
+        ndarrays_original = parameters_to_ndarrays(parameters_original)
 
-        if not results:
-            return None, {}
+        set_model_parameters(self.net, ndarrays_original)
+        loss, accuracy = test(self.net, self.valloader)
+        # return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
 
-        loss_aggregated = weighted_loss_avg(
-            [
-                (evaluate_res.num_examples, evaluate_res.loss)
-                for _, evaluate_res in results
-            ]
+        # Build and return response
+        status = Status(code=Code.OK, message="Success")
+        return EvaluateRes(
+            status=status,
+            loss=float(loss),
+            num_examples=len(self.valloader),
+            metrics={"accuracy": float(accuracy)},
         )
-        metrics_aggregated = {}
-        return loss_aggregated, metrics_aggregated
 
-    def evaluate(
-        self, server_round: int, parameters: Parameters
-    ) -> Optional[Tuple[float, Dict[str, Scalar]]]:
-        """Evaluate global model parameters using an evaluation function."""
-
-        # Let's assume we won't perform the global model evaluation on the server side.
-        return None
-
-    def num_fit_clients(self, num_available_clients: int) -> Tuple[int, int]:
-        """Return sample size and required number of clients."""
-        num_clients = int(num_available_clients * self.fraction_fit)
-        return max(num_clients, self.min_fit_clients), self.min_available_clients
-
-    def num_evaluation_clients(self, num_available_clients: int) -> Tuple[int, int]:
-        """Use a fraction of available clients for evaluation."""
-        num_clients = int(num_available_clients * self.fraction_evaluate)
-        return max(num_clients, self.min_evaluate_clients), self.min_available_clients
-
-# # Define metric aggregation function
-# def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
-#     # Multiply accuracy of each client by number of examples used
-#     accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
-#     examples = [num_examples for num_examples, _ in metrics]
-
-#     # Aggregate and return custom metric (weighted average)
-#     return {"accuracy": sum(accuracies) / sum(examples)}
+def client_fn(cid) -> FlowerClient:
+    # Load model and data (simple CNN, CIFAR-10)
+    net = Net().to(DEVICE)
+    trainloader, testloader = load_data(node_id=cid)
+    return FlowerClient(cid, net, trainloader, testloader)
 
 
-# Define strategy
-#strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
-strategy=FedCustom()#evaluate_metrics_aggregation_fn=weighted_average)
 
-# Start Flower server
-fl.server.start_server(
+# Start Flower client
+fl.client.start_client(
     server_address="127.0.0.1:5321",
-    config=fl.server.ServerConfig(num_rounds=rounds),
-    strategy=strategy,
+    client=client_fn(node_id),
 )
